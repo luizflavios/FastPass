9615863e6d2a0199b28179bca84e3e2d8948baaf
ngpbrasil <ngpbrasil@gmail.com>
2022-11-18 16:57:28 -0300

    fix: user image

--- src/main/java/br/com/newtonpaiva/fastpass/util/EmailGenerator.java
+++ src/main/java/br/com/newtonpaiva/fastpass/util/EmailGenerator.java
@@ -3,14 +3,18 @@ package br.com.newtonpaiva.fastpass.util;
 import br.com.newtonpaiva.fastpass.enums.EmailType;
 import br.com.newtonpaiva.fastpass.model.Email;
 import br.com.newtonpaiva.fastpass.model.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailGenerator {
 
     public static final String WELCOME_TO_FAST_PASS = "Welcome to Fast-Pass %s,\n\nYour activation code: %s\n\n%s!!";
-    private static final String ACTIVATE_URL = "http://localhost:8080/active-account";
+    private static final String ACTIVATE_URI = "/active-account";
     private static final String FORGOT_PASSWORD = "Hello, %s!\nThis is a security email, sent by Fast-Pass!\nYour new temporary password is: %s.";
+
+    @Value("${api.base.url}")
+    private String API_BASE_URL;
 
     public Email generate(User subject, EmailType type) {
 
@@ -39,7 +43,7 @@ public class EmailGenerator {
         return Email.builder()
                 .to(subject)
                 .type(EmailType.REGISTER)
-                .text(String.format(WELCOME_TO_FAST_PASS, subject.getFullName(), subject.getCode(), ACTIVATE_URL))
+                .text(String.format(WELCOME_TO_FAST_PASS, subject.getFullName(), subject.getCode(), API_BASE_URL + ACTIVATE_URI))
                 .subject(EmailType.REGISTER.getSubject())
                 .build();
     }
